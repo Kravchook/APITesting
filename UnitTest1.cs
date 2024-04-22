7d7ee9bfaa800baf8a87e36aef8c379ce06e92cd
Alexey Kravchenko <AlexeyKravchenko@coherentsolutions.com>
2024-04-22 13:31:40 +0300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace APITask10
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
